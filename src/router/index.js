52b80d22257785b6f8f2d3e1417462158fe3fd5e
oluwafemi <makurseme@gmail.com>
2021-12-11 19:21:52 +0100

    added titles

--- src/router/index.js
+++ src/router/index.js
@@ -2,31 +2,34 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
 
-const github = { template: '<div>github</div>'}
-
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta:{
+      title:'Home'
+    }
   },
   {
     path: '/Contact',
     name: 'Contact',
-    component: Contact
-  },
-  {
-    path: '/github',
-    beforeEnter(){
-       location.href='https://github.com/dev-makanju'  
-    },
-    component:github
-  },
+    component: Contact,
+    meta:{
+      title:'contact'
+    }
+  },  
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to , from , next) => {
+  let documentTitle = `Miracool - ${to.meta.title}`;
+  document.title = documentTitle;
+  next()
+});
 
 export default router
